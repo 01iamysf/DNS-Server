d42c267333fc765c49a1e0ce1a3df08800047280
01iamysf <01iamysf@gmail.com>
2026-03-13 14:33:04 -0400

    Initial DNS server project

---
+++ server.js
@@ -0,0 +1,35 @@
+const dgram = require("dgram");
+const dnsPacket = require("dns-packet");
+
+console.log("Starting DNS server...");
+
+const server = dgram.createSocket("udp4");
+
+server.on("message", (msg, rinfo) => {
+  console.log("Received DNS request from:", rinfo.address);
+
+  const query = dnsPacket.decode(msg);
+  console.log("Query for:", query.questions[0].name);
+
+  const response = dnsPacket.encode({
+    type: "response",
+    id: query.id,
+    questions: query.questions,
+    answers: [{
+      type: "A",
+      name: query.questions[0].name,
+      ttl: 300,
+      data: "192.168.1.10"
+    }]
+  });
+
+  server.send(response, rinfo.port, rinfo.address);
+});
+
+server.on("error", (err) => {
+  console.error("Server error:", err);
+});
+
+server.bind(5300, () => {
+  console.log("DNS server running on port 5353");
+});
